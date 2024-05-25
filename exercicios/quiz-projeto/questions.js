2ca40e98bbd1bee5834d528ca167a03751971cef
Gustavo dos Santos C. <gustavo.cssantos2002@gmail.com>
2024-05-25 11:34:10 -0300

    Initial commit

---
+++ exercicios/quiz-projeto/questions.js
@@ -0,0 +1,34 @@
+export default [
+  {
+      question: "Quanto é 1 + 2?",
+      answers: [
+          { option: "3", correct: true},
+          { option: "2", correct: false},
+          { option: "4", correct: false},
+      ]
+  },
+  {
+      question: "Quanto é 1 + 2?",
+      answers: [
+          { option: "3", correct: true},
+          { option: "2", correct: false},
+          { option: "4", correct: false},
+      ]
+  },
+  {
+      question: "Quanto é 1 + 2?",
+      answers: [
+          { option: "3", correct: true},
+          { option: "2", correct: false},
+          { option: "4", correct: false},
+      ]
+  },
+  {
+      question: "Quanto é 1 + 2?",
+      answers: [
+          { option: "3", correct: true},
+          { option: "2", correct: false},
+          { option: "4", correct: false},
+      ]
+  }
+];
